58111cb0af9cf4bc038ab92fa8c8f350f1cf7169
Ayushman Gupta <ayushman1998.1998@gmail.com>
2020-12-30 14:01:06 +0530

    created component (modal)

--- src/App.vue
+++ src/App.vue
@@ -2,11 +2,14 @@
   <input type="text" ref="name" />
   <button @click="submit">Submit</button>
   <p>{{ name }}</p>
+  <Modal />
 </template>
 
 <script>
+import Modal from "./components/Modal";
 export default {
   name: "App",
+  components: { Modal },
   data() {
     return {
       name: "",
@@ -16,12 +19,18 @@ export default {
     submit() {
       const { name } = this.$refs;
       this.name = name.value;
+      name.classList.add("input-class");
+      name.focus();
     },
   },
 };
 </script>
 
 <style>
+html, body {
+  margin: 0;
+  padding: 0;
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
